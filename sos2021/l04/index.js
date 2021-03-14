a936e76cd90804d7d28383ad867a97df4493f7aa
Jose Manuel <jgonzalezal37@gmail.com>
2021-03-14 10:20:35 +0000

    instalando el modulo de express

--- sos2021/l04/index.js
+++ sos2021/l04/index.js
@@ -1,3 +1,15 @@
 var cool = require("cool-ascii-faces");
+var express = require("express");
 
+var app = express();
+var port = 10000;
+
+app.get("/cool", (request,response) =>{
+	response.send(cool());
+	console.log("New request to /cool has arrived");
+});
+
+app.listen(port,() =>{
+	console.log("Server ready listening on port "+port)
+});
 console.log(cool());
